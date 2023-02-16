b765b8e2738f26316caea19cdeb442f486df4e1a
Enea Jahollari <jahollarienea14@gmail.com>
2023-02-16 19:15:19 +0100

    copied angular implementaion from repo

--- packages/ngx-signal-experimental/src/index.ts
+++ packages/ngx-signal-experimental/src/index.ts
@@ -1,1 +1,15 @@
-export * from './lib/ngx-signal-experimental';
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export { isSignal, Signal, ValueEqualityFn } from './lib/api';
+export { computed } from './lib/computed';
+export { setActiveConsumer } from './lib/internal';
+export { SettableSignal, signal } from './lib/signal';
+export { untrack } from './lib/untrack';
+export { Watch } from './lib/watch';
+export { effect } from './lib/effect';
